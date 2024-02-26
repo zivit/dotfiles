657389960f4bbabffdc43f13ec10d33c8b7631ce
zivit <zinc.vitaliy@gmail.com>
2024-02-27 00:57:01 +0200

    Updated packages and config files

--- .install-packages.sh
+++ .install-packages.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-pacman -S lazygit github-cli
+sudo pacman -S lazygit github-cli yay thefuck ripgrep fd lsd bat rustup steam github-cli unzip make ninja git-delta
+
+yay -S autojump bluemail
+rustup install stable
+pip install cmake-language-server
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
--- .zshrc
+++ .zshrc
@@ -124,6 +124,8 @@ alias ls="lsd"
 alias ll="lsd -l"
 
 source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme
+export LC_ALL=en_US.UTF-8
+export BAT_THEME="OneHalfDark"
 
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
