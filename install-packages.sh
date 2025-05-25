83bb4a5ab94484a84cf46e6e832d38fa6007816d
zivit <zinc.vitaliy@gmail.com>
2025-05-25 05:12:04 +0300

    Added packages to install-packages.sh

--- install-packages.sh
+++ install-packages.sh
@@ -7,8 +7,10 @@ sudo pacman -S \
     cloc \
     cmake \
     dunst \
+    dust \
     fd \
     firefox \
+    fzf \
     gcc \
     ghostty \
     git-delta \
@@ -46,8 +48,10 @@ sudo pacman -S \
     swaylock \
     thefuck \
     tldr \
+    tokei \
     ttf-jetbrains-mono-nerd \
     unzip \
+    yazi \
     wl-clipboard \
     zsh \
 
@@ -66,3 +70,4 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+git clone --recurse-submodules https://github.com/olets/zsh-abbr.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-abbr
